dac2884eb4fa47def5fd874744713b3c5bf1d652
Ryota-nino <ryota.swimmer@icloud.com>
2021-06-23 09:19:36 +0000

    機能作成のフォーム作成

---
+++ user_db_controller.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form action="https://back.easable.jp/easable-back/app/user/user_con.php" method="post">
+        <label>database1</label>
+        <input type="radio" name="database"/>
+        <label>database2</label>
+        <input type="radio" name="database"/>
+        <label>database3</label>
+        <input type="radio" name="database"/>
+        <input type="text" name="name" />
+        <input type="password" name="password" />
+        <input type="submit" name="insert"/>
+    </form>
+</body>
+</html>
