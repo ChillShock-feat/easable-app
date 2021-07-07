969f87052f8a11428b43bbda05f7aeedf178fde5
石丸大貴 <hirokiishimaru6132@gmail.com>
2021-07-07 11:54:34 +0900

    server追加処理完成

---
+++ js/add-project.js
@@ -0,0 +1,19 @@
+'use strict';
+{
+    window.onload = () =>{
+        let addBtn = document.getElementById('addserver');
+        let i = 1;
+        let parent = document.getElementById('addserver-parent');
+
+        addBtn.onclick = () =>{
+            let serverLabel = document.createElement('label');
+            let labelContent = document.createTextNode('サーバー名');
+            serverLabel.appendChild(labelContent);
+            let newServerData = document.createElement('input');
+            newServerData.type = 'text';
+            newServerData.id = 'servername-' + i;
+            parent.appendChild(serverLabel);
+            parent.appendChild(newServerData);
+        }
+    }
+}
